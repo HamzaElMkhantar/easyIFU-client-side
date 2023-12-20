fe2eeb104c7c2f4d49258031eb1f5e203a9f9c5f
Hamza El Mkhantar <hamzaelmkhantar4@gmail.com>
2023-12-20 03:46:56 +0100

    deployemnet test

--- src/pages/DashboardPages/DocumentInformation.jsx
+++ src/pages/DashboardPages/DocumentInformation.jsx
@@ -43,6 +43,7 @@ const DocumentInformation = () => {
       content: () => componentRef.current,
     });
     const [labelCount, setLabelCount] = useState(1)
+    console.log(labelCount)
 
   return (
     <div className="container pt-3">
@@ -50,14 +51,25 @@ const DocumentInformation = () => {
           <Link style={{height:'35px'}} to='/dashboard/documents' className='label-info-link'><ArrowBackIcon /> Back</Link>
           <button style={{height:'35px'}} onClick={handlePrint}><PrintIcon style={{fontSize:'40px', color:'#8C2E2C'}} /></button>
         </div>
+        <div>
+          <div  className='pt-4'>
+            <label htmlFor="labelCount">How many label you want</label>
+            <br/>
+            <input style={{maxWidth:'150px'}}
+                    id='labelCount' 
+                    type='number'
+                    min="1"
+                    value={labelCount}
+                    onChange={(e) => setLabelCount(e.target.value)}/>
+          </div>
           <div className='document-content pt-4' >
            {!documentByIdRequest 
-           ?<div ref={componentRef} className="A4-content" style={{display:'flex', flexWrap:'wrap', justifyContent:'space-evenly', alignItems:'flex-start'}}>
-           {[...Array(5)].map((_, index) => (
+           ?<div ref={componentRef} className="A4-content pt-1" style={{display:'flex', flexWrap:'wrap', justifyContent:'space-evenly', alignItems:'flex-start'}}>
+           {[...Array(parseInt(labelCount))].map((_, index) => (
              <img
                key={index}
                style={{ width: `${size}`, height: '', border: '1px solid black' }}
-               className="card-img-top p-1 my-1"
+               className="card-img-top p-1 m-1"
                src={`${process.env.REACT_APP_BASE_URL}/assets/documents/${documentInfo && documentInfo?.imageUrl}`}
                alt={documentInfo && documentInfo?.projectName}
              />
@@ -73,6 +85,7 @@ const DocumentInformation = () => {
             visible={true}/>}
             
           </div>
+        </div>
     </div>
   );
 };
